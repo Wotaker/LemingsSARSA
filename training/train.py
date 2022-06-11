f4a521fd770156c703f3802108ceb9005d7c970f
wotaker <wojciech.ciezobka@gmail.com>
2022-06-11 15:55:31 +0200

    Experiments till IV - finished

--- training/train.py
+++ training/train.py
@@ -17,7 +17,7 @@ from training.basic import run_basic
 SEED = 420
 LEVEL = lvl_small
 STOCHASTIC_WIND = False
-EXTRA_MOVES = True
+EXTRA_MOVES = False
 
 MY_ENV = gym.make(
     "LemingsEnv-v1", 
@@ -29,8 +29,8 @@ MY_ENV = gym.make(
 
 # ===== Learning Parameters ========
 # ----------------------------------
-ALGORITHM = 'q'         # choose from ['q', 'sarsa', 'basic']
-EPISODES = 5000
+ALGORITHM = 'sarsa'         # choose from ['q', 'sarsa', 'basic']
+EPISODES = 3000
 LEARNING_RATE = 0.5
 EXPERIMENT_RATE = 0.05
 DISCOUNT_FACTOR = 0.95
@@ -40,7 +40,7 @@ DECAY = 0.0
 # ===== Logging Parameters =========
 # ----------------------------------
 RUNS = 5
-PLOT_SAVE_PATH = f"results\\plots\\plot_{ALGORITHM}_{LEVEL.name}_{RUNS}_mv.png"
+PLOT_SAVE_PATH = f"results\\plots\\plot_{ALGORITHM}_{LEVEL.name}_{RUNS}.png"
 LOGS_DIR_PATH = "logs\\logs"
 VERBOSE = False                 # Renders the game states if set as True
 LOG_EP_EVRY = 1
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     plotter = Plotter(
         logs_dir_path=LOGS_DIR_PATH, 
         save_plot_path=PLOT_SAVE_PATH,
-        n_action_buckets=100,
+        n_action_buckets=200,
         episodes_moving_average=EPISODE_WINDOW
     )
 
